4fa1793bd780983e4d3d33a6d17ad45039d0c740
Martin Berg <mbrrg@users.noreply.github.com>
2018-08-09 10:04:22 +0200

    Add packaging details.

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+VERSION = '0.1.0'
+DOWNLOAD_URL = \
+    'https://github.com/mbrrg/pyspcwebgw/archive/{}.zip'.format(VERSION)
+
+setup(
+    name='pyspcwebgw',
+    packages=['pyspcwebgw'],
+    version='0.1.0',
+    description='A Python library for communicating with SPC Web Gateway.',
+    author='Martin Berg',
+    author_email='mbrrg@users.noreply.github.com',
+    license='MIT',
+    url='https://github.com/mbrrg/pyspcwebgw',
+    download_url='https://github.com/mbrrg/pyspcwebgw/archive/v',
+    install_requires=['aiohttp', 'asynccmd'],
+    python_requires='>=3.5',
+    scripts=['scripts/spcwebgw-console'],
+    keywords=['spc', 'vanderbilt', 'web gateway'],
+    classifiers=[
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Topic :: Home Automation'
+    ],
+)
