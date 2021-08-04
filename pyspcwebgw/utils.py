788c49c8a455fd8986a3998f3f4032ee0b852fd6
Ian Duffy <ian@ianduffy.ie>
2021-08-04 01:24:29 +0100

    Add retry logic

--- pyspcwebgw/utils.py
+++ pyspcwebgw/utils.py
@@ -17,27 +17,42 @@ def _load_enum(enum, value, default=None):
 
 async def async_request(session, url, **kwargs):
     """Do a web request and manage response."""
-    try:
-        with async_timeout.timeout(10):
-            _LOGGER.debug("Sending %s to %s", kwargs, url)
-            response = await session(url, **kwargs)
-        if response.status != 200:
-            _LOGGER.error("HTTP status %d, response %s.",
-                          response.status, (await response.text()))
+    retry = -1
+    retries = 25
+    while True:
+        retry += 1
+        if retry > 0:
+            delay = float(retry * retry) / 10.0
+            _LOGGER.info('Sleeping %0.2f seconds for exponential backoff', delay)
+            await asyncio.sleep(delay)
+        try:
+            with async_timeout.timeout(20):
+                _LOGGER.debug("Sending %s to %s", kwargs, url)
+                response = await session(url, **kwargs)
+        except (aiohttp.ClientError, asyncio.TimeoutError) as err:
+            if retry < retries:
+                _LOGGER.warning('Retrying', err)
+                continue
             return False
+        except ValueError as err:
+            _LOGGER.warning('ValueError from aiohttp: redirect to non-http or https', err)
+            raise False
+        except RuntimeError as err:
+            _LOGGER.warn('RuntimeError from aiohttp: session closed', err)
+            raise False
+        # Handle non 2xx status code and retry if possible
+        if 500 <= response.status < 600 and retry < retries:
+            if retry < retries:
+                _LOGGER.warning('Retrying because of: %d status' % response.status)
+                continue
+            else:
+                _LOGGER.error("HTTP status %d, response %s.",
+                              response.status, (await response.text()))
+                return False
         result = await response.json()
-    except asyncio.TimeoutError:
-        _LOGGER.error("Timeout getting SPC data from %s.", url)
-        return False
-    except aiohttp.ClientError:
-        _LOGGER.error("Error getting SPC data from %s.", url)
-        return False
-    else:
-        _LOGGER.debug("HTTP request response: %s", result)
 
-    if result['status'] != 'success':
-        _LOGGER.error(
-            "SPC Web Gateway call unsuccessful for resource: %s", url)
-        return False
-
-    return result
+        if result['status'] != 'success':
+            _LOGGER.error(
+                "SPC Web Gateway call unsuccessful for resource: %s", url)
+            return False
+        return result
