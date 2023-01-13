8da6f9e526360a1aade2ac9835dee971fb725609
Jakob Schlyter <jakob@kirei.se>
2023-01-13 08:01:06 +0100

    reformat

--- examples/example1.py
+++ examples/example1.py
@@ -1,32 +1,35 @@
 import asyncio
 
 import aiohttp
+
 from pyspcwebgw import SpcWebGateway
 
-
-API_URL = 'http://192.168.1.10:8088'
-WS_URL = 'ws://192.168.1.10:8088'
+API_URL = "http://192.168.1.10:8088"
+WS_URL = "ws://192.168.1.10:8088"
 
 
 async def callback(entity):
-    print('SPC update - ' + str(entity))
+    print("SPC update - " + str(entity))
 
 
 async def init(loop):
     # logging.basicConfig(level=logging.DEBUG)
 
     websession = aiohttp.ClientSession(loop=loop)
-    spc = SpcWebGateway(loop, websession, api_url=API_URL, ws_url=WS_URL,
-                        async_callback=callback)
+    spc = SpcWebGateway(
+        loop, websession, api_url=API_URL, ws_url=WS_URL, async_callback=callback
+    )
 
     result = await spc.async_load_parameters()
     if result is False:
-        print('Failed to connect to SPC Web Gateway.')
+        print("Failed to connect to SPC Web Gateway.")
         exit(1)
 
     # Print basic info about the panel
-    print(f"Connected to SPC {spc.info['variant']}, serial no "
-          f"{spc.info['sn']} running firmware {spc.info['version']}.")
+    print(
+        f"Connected to SPC {spc.info['variant']}, serial no "
+        f"{spc.info['sn']} running firmware {spc.info['version']}."
+    )
 
     # Connect the websocket
     spc.start()
@@ -35,7 +38,8 @@ async def init(loop):
     for _, area in spc.areas.items():
         print(area)
         for zone in area.zones:
-            print('   ' + str(zone))
+            print("   " + str(zone))
+
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
--- pyspcwebgw/zone.py
+++ pyspcwebgw/zone.py
@@ -1,6 +1,6 @@
 import logging
 
-from .const import ZoneInput, ZoneType, ZoneStatus
+from .const import ZoneInput, ZoneStatus, ZoneType
 from .utils import _load_enum
 
 _LOGGER = logging.getLogger(__name__)
@@ -8,20 +8,24 @@ _LOGGER = logging.getLogger(__name__)
 
 class Zone:
     """Represents an SPC alarm system zone."""
-    SUPPORTED_SIA_CODES = ('ZO', 'ZC', 'ZX', 'ZD', 'ZM', 'BA',
-                           'BB', 'BU', 'BR', 'BC')
+
+    SUPPORTED_SIA_CODES = ("ZO", "ZC", "ZX", "ZD", "ZM", "BA", "BB", "BU", "BR", "BC")
 
     def __init__(self, area, spc_zone):
-        self._id = spc_zone['id']
-        self._name = spc_zone['zone_name']
+        self._id = spc_zone["id"]
+        self._name = spc_zone["zone_name"]
         self._area = area
 
         self.update(spc_zone)
 
     def __str__(self):
-        return '{id}: {name} ({type}). Input: {inp}, status: {status}'.format(
-            id=self.id, name=self.name, type=self.type,
-            inp=self.input, status=self.status)
+        return "{id}: {name} ({type}). Input: {inp}, status: {status}".format(
+            id=self.id,
+            name=self.name,
+            type=self.type,
+            inp=self.input,
+            status=self.status,
+        )
 
     @property
     def id(self):
@@ -50,13 +54,13 @@ class Zone:
     def update(self, spc_zone, sia_code=None):
         _LOGGER.debug("Update zone %s", self.id)
 
-        self._type = _load_enum(ZoneType, spc_zone['type'])
-        self._status = _load_enum(ZoneStatus, spc_zone['status'])
+        self._type = _load_enum(ZoneType, spc_zone["type"])
+        self._status = _load_enum(ZoneStatus, spc_zone["status"])
 
-        input = _load_enum(ZoneInput, spc_zone['input'])
-        
-        if sia_code == 'ZO' and input == ZoneInput.CLOSED:
+        input = _load_enum(ZoneInput, spc_zone["input"])
+
+        if sia_code == "ZO" and input == ZoneInput.CLOSED:
             # work around race condition for wireless sensors
             input = ZoneInput.OPEN
-        
+
         self._input = input
